2631be7993e4a3fc709e6fe1ef55faa60c36719a
Achard Jean-Francis <jean-francis.achard@epitech.eu>
2025-12-07 02:34:02 +0100

    Init portfolio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Hello Vite + React!
+      </h1>
+    </>
+  )
+}
+
+export default App
